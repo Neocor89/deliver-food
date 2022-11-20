a26394160b9d435fbbc07c9183a2b30db3e8d956
Ben <ncor89b@gmail.com>
2022-11-20 20:10:28 +0100

    Dishes info recovering and Format numbers libraries added

--- components/DishRow.js
+++ components/DishRow.js
@@ -1,7 +1,25 @@
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
+import { NumericFormat } from "react-number-format";
+import tw from "twrnc";
 
 const DishRow = ({ id, name, short_description, price, image }) => {
-  return <div>DishRow</div>;
+  /*
+  : Libraries React Format Number added 
+  : For convert price number to string format
+  */
+
+  return (
+    <TouchableOpacity>
+      <View>
+        <Text style={tw`text-lg mb-1`}>{name}</Text>
+        <Text style={tw`text-gray-400`}>{short_description}</Text>
+        <Text style={tw`text-gray-400`}>
+          {/* <CurrencyFormat quantity={price} currency="GBP" /> */}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
 };
 
 export default DishRow;
