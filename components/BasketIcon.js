7b874e723f67708faa941fc684c14a8de990008a
Ben <ncor89b@gmail.com>
2022-11-26 01:00:12 +0100

    added new favicon and update layout payment button

--- components/BasketIcon.js
+++ components/BasketIcon.js
@@ -11,17 +11,18 @@ const BasketIcon = () => {
   const items = useSelector(selectBasketItems);
   const navigation = useNavigation();
   const basketTotal = useSelector(selectBasketTotal);
+
+  //TODO : >> Continuation of the creation of the cart features and the layout
+
   return (
     <View style={tw`absolute bottom-10 w-full z-50 mx-auto`}>
       <TouchableOpacity
-        style={tw`mx-5 bg-[#06d6a0] p-4 rounded-lg flex-row items-center px-3 py-3`}
+        style={tw`mx-5 bg-[#06d6a0] p-4 rounded-lg flex-row items-center px-3 py-3 ml-2`}
       >
-        <Text
-          style={tw`text-white font-extrabold text-lg bg-[#0EB88C] px-2 mr-2`}
-        >
+        <Text style={tw`text-white font-extrabold text-lg bg-[#0EB88C] px-2`}>
           {items.length}
         </Text>
-        <Text>View Basket</Text>
+        <Text style={tw`text-[16px] font-medium`}>View Basket</Text>
         <Text style={tw`text-lg text-white font-extrabold ml-2`}>
           <NumericFormat displayType="text" value={basketTotal} prefix="$" />
         </Text>
