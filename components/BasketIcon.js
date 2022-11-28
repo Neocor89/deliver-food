de976fde7ae5130cbd73cf0785115faaff43c6e5
Ben <ncor89b@gmail.com>
2022-11-28 21:31:06 +0100

    create Basket component and connection with redux

--- components/BasketIcon.js
+++ components/BasketIcon.js
@@ -12,12 +12,10 @@ const BasketIcon = () => {
   const navigation = useNavigation();
   const basketTotal = useSelector(selectBasketTotal);
 
-  //TODO : >> Continuation of the creation of the cart features and the layout
-
   return (
     <View style={tw`absolute bottom-10 w-full z-50 mx-auto`}>
       <TouchableOpacity
-        onPress={navigation.goBack}
+        onPress={() => navigation.navigate("Basket")}
         style={tw`mx-5 bg-[#06d6a0] p-4 rounded-lg flex-row items-center px-3 py-3 ml-2`}
       >
         <Text style={tw`text-white font-extrabold text-lg bg-[#0EB88C] px-2`}>
