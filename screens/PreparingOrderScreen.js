d2bf13ba97fa73cf2b40e63898f7f03b92694d50
Ben <ncor89b@gmail.com>
2022-12-20 20:55:09 +0100

    add gif animation for loading order

--- screens/PreparingOrderScreen.js
+++ screens/PreparingOrderScreen.js
@@ -1,10 +1,18 @@
 import { View, Text } from "react-native";
 import React from "react";
+import tw from "twrnc";
+import * as Animatable from "react-native-animatable";
 
 const PreparingOrderScreen = () => {
   return (
-    <View>
-      <Text>PreparingOrderScreen</Text>
+    <View style={tw`bg-[#06d6a0] flex-1 justify-center items-center`}>
+      <Animatable.Image
+        source={require("../assets/deliverfood-preparingOrder.gif")}
+        animation="slideInUp"
+        iterationCount={1}
+        style={tw`h-90 w-90 rounded-[8px]`}
+      />
+      {/* <Text>PreparingOrderScreen</Text> */}
     </View>
   );
 };
